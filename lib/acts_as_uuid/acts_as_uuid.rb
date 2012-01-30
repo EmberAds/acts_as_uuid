050defe25f8829d47704d7b8872488355c4faee4
Cristiano Betta <cbetta@gmail.com>
2012-01-30 18:43:30 +0000

    UUID is now generated before validation instead of before create

--- lib/acts_as_uuid/acts_as_uuid.rb
+++ lib/acts_as_uuid/acts_as_uuid.rb
@@ -1,11 +1,11 @@
 module ActsAsUUID
   def self.included klass
-    klass.before_create :generate_uuid
+    klass.before_validation :generate_uuid
     klass.validates :id, :uniqueness => true
     klass.primary_key = :id
   end
 
   def generate_uuid
-    self.id = UUID.generate
+    self.id ||= UUID.generate
   end
 end
