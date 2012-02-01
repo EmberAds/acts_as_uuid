370e737d6b4dcdfadef56605fd8e40a36799cad9
Cristiano Betta <cbetta@gmail.com>
2012-02-01 15:34:12 +0000

    Lots of fixes to clean up. Bumped version to 0.1.0

--- lib/acts_as_uuid/acts_as_uuid.rb
+++ lib/acts_as_uuid/acts_as_uuid.rb
@@ -1,23 +1,23 @@
 module ActsAsUUID
   def self.included klass
-    klass.before_validation :generate_uuids
+    klass.before_validation :generate_uuid
     klass.validates :id, :uniqueness => true
     klass.primary_key = :id
-
-
-    def klass.uuid_on field, options = {}
-      @@fields ||= Array.new
-      @@fields << field
-      self.validates field, :uniqueness => true
-      self.primary_key = field if options[:as] == :primary_key
-    end
+    klass.extend ClassMethods
   end
 
-  def generate_uuids
-    @@fields.each do |field|
-      self[field] ||= UUID.generate
-    end
+  def generate_uuid
+    self[self.class.acts_as_uuid_field] ||= UUID.generate
   end
 
+  module ClassMethods
+    attr_accessor :acts_as_uuid_field
+
+    def uuid_on uuid_field, options = {}
+      self.acts_as_uuid_field = uuid_field
+      self.validates uuid_field, :uniqueness => true
+      self.primary_key = uuid_field if options[:as] == :primary_key
+    end
+  end
 
 end
